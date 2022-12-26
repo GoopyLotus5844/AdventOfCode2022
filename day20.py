47b323d589179d0136349312c1b27c80c14364f8
hank <hpstennes@gmail.com>
2022-12-25 18:25:31 -0600

    Day 20 part 2

--- day20.py
+++ day20.py
@@ -4,33 +4,34 @@ data = read("input.txt", ['\n'])
 
 def answer(values):
     for i, val in enumerate(values):
-        if val == 0:
+        if val[1] == 0:
             zero_index = i
             break
-    value_1 = values[(zero_index + 1000) % len(data)]
-    value_2 = values[(zero_index + 2000) % len(data)]
-    value_3 = values[(zero_index + 3000) % len(data)]
+    value_1 = values[(zero_index + 1000) % len(data)][1]
+    value_2 = values[(zero_index + 2000) % len(data)][1]
+    value_3 = values[(zero_index + 3000) % len(data)][1]
     return value_1 + value_2 + value_3
 
-# data = [val * 811589153 for val in data]
-values = data[:]
+data = [val * 811589153 for val in data]
+values = [(i, val) for i, val in enumerate(data)]
 index = 0
 
-for _ in range(1):
-    for current in data:
+for _ in range(10):
+    for id in range(len(data)):
         while True:
-            if values[index] == current: break
+            if values[index][0] == id: break
             index += 1
             if index == len(data): index = 0
         
         #moving current from index index to index dest
-        base_dest = index + current
-        dest = base_dest % len(data)
-        if base_dest < 0: dest -= -base_dest // len(data) + 1
-        elif base_dest >= len(data): dest += base_dest // len(data)
+        offset = values[index][1]
+        base_dest = index + offset
 
-        values.pop(index)
-        values.insert(dest, current)
-        if dest < index: index += 1
+        if base_dest < 0: dest = (len(data) - 2) - (-base_dest - 1) % (len(data) - 1)
+        elif base_dest >= len(data): dest = 1 + (base_dest - len(data)) % (len(data) - 1)
+        else: dest = base_dest
+
+        item = values.pop(index)
+        values.insert(dest, item)
 
 print(answer(values))
